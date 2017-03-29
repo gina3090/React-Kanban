7eda7afa31250106ed70ebe18f3cf0c837f5339a
ginagala3090 <ginagala3090@users.noreply.github.com>
2017-03-29 13:18:28 -1000

    modified done

--- client/my-app/src/components/Done.js
+++ client/my-app/src/components/Done.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Card from './Card';
 
-const Done = (props) => (
-  <div className="Done">
-    <h3>{props.doneTitle}</h3>
-  </div>
-);
+class Done extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="done-cards">
+        {this.props.cards
+          .filter((card) =>
+            card.status === 'done'  
+          )
+          .map((card) => {
+            return (
+              <Card
+                title={card.title}
+                priority={card.priority}
+                createdBy={card.createdBy}
+                assignedTo={card.assignedTo}
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
+}
 
 export default Done;
