a35c9f590e264aa25416265701c35a24272b6205
ginagala3090 <ginagala3090@users.noreply.github.com>
2017-03-29 13:17:46 -1000

    modified queue

---
+++ client/my-app/src/components/InQueue.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Card from './Card';
+
+class InQueue extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="queue-cards">
+        {this.props.cards
+          .filter((card) =>
+            card.status === 'queue'
+          )
+          .map((card) => {
+            return (
+              <Card
+                title={card.title}
+                priority={card.priority}
+                createdBy={card.createdBy}
+                assignedTo={card.assignedTo}
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
+}
+
+export default InQueue;
