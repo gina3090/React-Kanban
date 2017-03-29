c5c28c1a780009486e37b3b1b9883d26bbd767a8
ginagala3090 <ginagala3090@users.noreply.github.com>
2017-03-29 13:25:18 -1000

    created reducers folder

---
+++ client/my-app/src/reducers/index.js
@@ -0,0 +1,27 @@
+import { ADD_CARD } from '../actions';
+
+const initialState = {
+  cards: []
+};
+
+function cards(state = initialState, action) {
+  switch(action.type) {
+    case ADD_CARD:
+      return Object.assign({}, state, {
+        cards: [
+          ...state.cards,
+          {
+            title: action.title,
+            priority: action.priority,
+            status: action.status,
+            createdBy: action.createdBy,
+            assignedTo: action.assignedTo
+          }
+        ]
+      });
+    default:
+      return state;
+  }
+}
+
+export default cards;
