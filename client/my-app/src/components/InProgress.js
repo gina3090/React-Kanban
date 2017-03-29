568fef7d1361f4a9c2d7a2552a2e8058ab5c9f4d
ginagala3090 <ginagala3090@users.noreply.github.com>
2017-03-29 13:18:08 -1000

    modified progress

---
+++ client/my-app/src/components/InProgress.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Card from './Card';
+
+class InProgress extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="progress-cards">
+        {this.props.cards
+          .filter((card) =>
+            card.status === 'progress'
+          )
+          .map((card) => {
+            return (
+              <Card
+                title={card.title}
+                priority={card.priority}
+                createdBy={card.createdBy}
+                assignedTo={card.assignedTo}
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
+}
+
+export default InProgress;
