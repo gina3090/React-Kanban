e53c884c933b1803b6b7fe74e0b8e7cdfaa838f1
ginagala3090 <ginagala3090@users.noreply.github.com>
2017-04-04 17:28:49 -1000

    simplified code in actions folder

--- client/my-app/src/actions/index.js
+++ client/my-app/src/actions/index.js
@@ -3,10 +3,10 @@ export const ADD_CARD = 'ADD_CARD';
 export function addCard(title, priority, status, createdBy, assignedTo) {
   return {
     type: ADD_CARD,
-    title: title,
-    priority: priority,
-    status: status,
-    createdBy: createdBy,
-    assignedTo: assignedTo
+    title,
+    priority,
+    status,
+    createdBy,
+    assignedTo
   };
 }
