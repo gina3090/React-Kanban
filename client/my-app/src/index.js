4a0c30fcbe84fbe7700864245788a9c90e795956
ginagala3090 <ginagala3090@users.noreply.github.com>
2017-04-04 17:25:37 -1000

    changed reducer file name

--- client/my-app/src/index.js
+++ client/my-app/src/index.js
@@ -5,13 +5,13 @@ import './index.css';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import reducers from './reducers';
+import cards from './reducers';
 
 import ReduxThunk from 'redux-thunk';
 // import { createDevTools } from 'redux-devtools';
 
 let store = createStore(
-  reducers,
+  cards,
   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(ReduxThunk)
 );
