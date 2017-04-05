1510e8aabf2b7748e901e64b3dcb9ea727b4ba90
ginagala3090 <ginagala3090@users.noreply.github.com>
2017-04-04 17:21:02 -1000

    modified postCard function

--- client/my-app/src/lib/postCard.js
+++ client/my-app/src/lib/postCard.js
@@ -1,4 +1,4 @@
-module.exports = function postCard() {
+module.exports = function postCard(card) {
   return new Promise((resolve, reject) => {
     function postCardData() {
       let data = JSON.parse(this.responseText);
@@ -6,8 +6,8 @@ module.exports = function postCard() {
     }
     const postCard = new XMLHttpRequest();
     postCard.addEventListener("load", postCardData);
-    postCard.open("POST", "/api/kanban/new");
-    postCard.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    postCard.send();
+    postCard.open("POST", "/api/kanban/new", true);
+    postCard.setRequestHeader("Content-Type", "application/json");
+    postCard.send(JSON.stringify(card));
   });
 };
