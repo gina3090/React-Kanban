bccaefe9f0c447717e0ea15e604c8a2e3da84d3a
ginagala3090 <ginagala3090@users.noreply.github.com>
2017-04-04 17:02:40 -1000

    added bodyParser.json

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const app = express();
+
 const card = require('./routes/card');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({extended: true}));
 
 app.use('/api/kanban', card);
 
