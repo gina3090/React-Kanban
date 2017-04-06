fdace46b8492148fde79e0dd685f331f7fb2e34d
ginagala3090 <ginagala3090@users.noreply.github.com>
2017-04-05 14:53:39 -1000

    added editCard and removeCard to actions

--- client/my-app/src/actions/index.js
+++ client/my-app/src/actions/index.js
@@ -1,4 +1,6 @@
 export const ADD_CARD = 'ADD_CARD';
+export const EDIT_CARD = 'EDIT_CARD';
+export const REMOVE_CARD = 'REMOVE_CARD';
 
 export function addCard(title, priority, status, createdBy, assignedTo) {
   return {
@@ -8,5 +10,24 @@ export function addCard(title, priority, status, createdBy, assignedTo) {
     status,
     createdBy,
     assignedTo
+  };
+}
+
+export function editCard(id, title, priority, status, createdBy, assignedTo) {
+  return {
+    type: EDIT_CARD,
+    id,
+    title,
+    priority,
+    status,
+    createdBy,
+    assignedTo
+  };
+}
+
+export function removeCard(id) {
+  return {
+    type: REMOVE_CARD,
+    id
   };
 }
