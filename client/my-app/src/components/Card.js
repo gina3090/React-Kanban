d2295477fd3628380e73131d81ee1ce682d2b824
ginagala3090 <ginagala3090@users.noreply.github.com>
2017-04-05 14:58:56 -1000

    added handleEdit and handleDelete

--- client/my-app/src/components/Card.js
+++ client/my-app/src/components/Card.js
@@ -3,6 +3,30 @@ import React, { Component } from 'react';
 class Card extends Component {
   constructor(props) {
     super(props);
+
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleEdit(event) {
+    event.preventDefault();
+
+    this.props.editCard({
+      id: this.props.id,
+      title: this.props.title,
+      priority: this.props.priority,
+      status: this.props.status,
+      createdBy: this.props.createdBy,
+      assignedTo: this.props.assignedTo
+    });
+  }
+
+  handleDelete(event) {
+    event.preventDefault();
+
+    this.props.removeCard({
+      id: this.props.id
+    });
   }
 
   render() {
@@ -12,8 +36,12 @@ class Card extends Component {
         <p>Priority: {this.props.priority}</p>
         <p>Created by: {this.props.createdBy}</p>
         <p>Assigned to: {this.props.assignedTo}</p>
-        <button className="edit-button">Edit</button>
-        <button className="delete-button">Delete</button>
+        <div className="edit-button">
+          <button onClick={this.handleEdit}>Edit</button>
+        </div>
+        <div className="delete-button">
+          <button onClick={this.handleDelete}>Delete</button>
+        </div>
       </div>
     );
   }
